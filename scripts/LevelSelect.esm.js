2137e010b924a8beed99adbe2c78f2b7bb3a3de2
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-06 19:10:38 +0100

    Update

--- scripts/LevelSelect.esm.js
+++ scripts/LevelSelect.esm.js
@@ -1,4 +1,5 @@
-import { Common, HIDDEN_SCREEN } from './Common.esm.js';
+import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from './Common.esm.js';
+import { canvas } from './Canvas.esm.js';
 
 const gameLevels = [
     {
@@ -34,7 +35,7 @@ class LevelSelect extends Common {
 
     buttonOnClickHandler(event) {
         this.changeVisibilityScreen(this.element, HIDDEN_SCREEN);
-        // pokaż planszę z grą
+        this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN);
     }
 }
 
